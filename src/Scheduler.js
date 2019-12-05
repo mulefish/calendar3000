6c8a64002b400b6ec8d3acd824a5d6e7f126ae0d
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.local>
2019-12-05 10:56:05 -0600

    first commit

---
+++ src/Scheduler.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+
+
+class Scheduler extends React.Component {
+
+  render() { 
+    return (
+      <div className="App">
+        Scheduler
+      </div>
+    );
+  }
+
+} 
+export default Scheduler;
+
+
+/* 
+class Scheduler extends React.Component {
+
+  render() { 
+    return (
+      <div className="App">
+Scheduler
+      </div>
+    );
+  }
+
+} 
+export default Scheduler;
+*/ 
