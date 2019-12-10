96a6153dfec0a3ba2692577142648318c6cf9a79
Sharon Saadia <Sharon.Saadia@perficient.com>
2019-12-10 16:31:25 -0500

    add modal

--- src/Cell.js
+++ src/Cell.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Modal from './Modal';
 
+import './Cell.css';
 
 class Cell extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      show: false
+      show: false,
+      newTitle: "",
+      titleColor: "#adadad", //Grey
+      comment: "",
+      titles: []
     }
   } 
 
@@ -25,14 +30,101 @@ class Cell extends React.Component {
     })
   }
 
+  resetTitle = () => {
+    this.setState({
+      newTitle: ""
+    })
+  }
+
+  resetTitleColor = () => {
+    this.setState({
+      titleColor: "#adadad" //Grey
+    })
+  }
+
+  resetComment = () => {
+    this.setState({
+      comment: ""
+    })
+  }
+
+  handleAdd = () => {
+    //Add new title
+    let title = this.state.newTitle
+    if(title.trim().length > 0) {
+      this.setState({
+        titles: [...this.state.titles, {title: title, color: this.state.titleColor, comment: this.state.comment}]
+      })
+    }
+    //reset modal fields
+    this.resetTitle()
+    this.resetTitleColor()
+    this.resetComment()
+    //close
+    this.hideModal()
+  }
+
+  handleNewTitleChange = e => {
+    let value = e.target.value
+    this.setState({
+      newTitle: value
+    })
+  }
+
+  handleTitleColorChange = e => {
+    let value = e.target.value
+    this.setState({
+      titleColor: value
+    })
+  }
+
+  handleCommentChange = e => {
+    let value = e.target.value
+    this.setState({
+      comment: value
+    })
+  }
+
   render() { 
     const w = {'width': this.props.width, 'height': this.props.height }
-    console.log( "jhjhjhjhhjkc " + JSON.stringify( this.props.height ) )
+    const bgColor = {'backgroundColor': this.state.titleColor}
+    let aryTitles = [] 
+    this.state.titles.forEach((t, i ) => { 
+      const key = "t" + i 
+      const bgColorCell = {'backgroundColor': t.color}
+      aryTitles.push( <div key={key} style={bgColorCell}><span className="title-text">{t.title}</span></div>)
+    })
+
     return (    
         <td className='day' style={w} onClick={e => { this.showModal()}}>
-            {this.props.data}  
-            <Modal show={this.state.show}  handleClose={this.hideModal}>
-              <div>Hello</div>
+            {this.props.data}
+            {aryTitles.length>0 ?
+              <div className="titles-wrapper">
+                {aryTitles}
+              </div>
+            :null}  
+            <Modal show={this.state.show}  handleClose={this.hideModal} handleAdd={this.handleAdd}>
+            <div className="modal-content">Create a new event:</div>
+            <div className="modal-event-input">
+              <input type="text" name="newTitle" value={this.state.newTitle} onChange={e => {this.handleNewTitleChange(e)}} style={{'width': 100+'%'}}/>
+            </div>
+            <div className="modal-event-color">
+              <label>Color:</label><br></br>
+              <select  value={this.state.titleColor} onChange={this.handleTitleColorChange}>
+                <option value='#6393e0'>Blue</option>
+                <option value='#88b972'>Green</option>
+                <option value='#eba760'>Yellow</option>
+                <option value='#aa0000'>Red</option>
+                <option value='#adadad'>Grey</option>
+              </select>
+              <span className="color-box" style={bgColor}></span>
+            </div>
+            <div>
+            <label>
+              Comments: <br></br>
+              <textarea value={this.state.comment} onChange={this.handleCommentChange} cols={40} rows={10} />
+            </label>
+            </div>
             </Modal>
         </td>
     );
--- src/Modal.js
+++ src/Modal.js
@@ -1,14 +1,29 @@
 import React from "react";
 
+import './Modal.css';
+
 export default class Modal extends React.Component {
 
+    handleClickModalUnderlay = e => {
+        let targetClassName = e.target.className
+        if(targetClassName && targetClassName === "modal-underlay") {
+            this.props.handleClose()
+        }
+      }
+      
   render() {
     if(!this.props.show){
         return null;
     }
-    return <div>
-                <div>{this.props.children}</div>
-                <div><button onClick={this.props.handleClose}>Close</button></div>
+    return <div className="modal-underlay" onClick={e => { this.handleClickModalUnderlay(e)}}>
+                <div className="modal-main">
+                    <div className="modal-inner">
+                        <div>{this.props.children}</div>
+                        <div className="modal-buttons">
+                            <div><button onClick={this.props.handleAdd}>OK</button><button onClick={this.props.handleClose}>Close</button></div>
+                        </div>
+                    </div>
+                </div>
             </div>;
   }
 }
